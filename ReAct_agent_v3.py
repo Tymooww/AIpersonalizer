abf321e4508249bf41c565393446cb5337b553d8
timo.sevarts <timo.sevarts@iodigital.com>
2025-10-06 16:02:16 +0200

    Added tool for personalizing image

--- ReAct_agent_v3.py
+++ ReAct_agent_v3.py
@@ -1,10 +1,14 @@
 """This agent retrieves all pages (with uid 'page') from ContentStack and personalizes them using tools."""
-from typing import TypedDict
+from typing import Annotated, Sequence, TypedDict
 from dotenv import load_dotenv
 from langchain_core.tools import tool
 from langchain_litellm import ChatLiteLLM
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import BaseMessage
+from langchain_core.messages import ToolMessage
+from langchain_core.messages import SystemMessage
+from langgraph.graph.message import add_messages
 import os
 import requests
 from pymongo import MongoClient
@@ -18,8 +22,7 @@ def initialize_config():
     llm_config = ChatLiteLLM(
         model=os.getenv("BONZAI_MODEL"),
         api_key=os.getenv("BONZAI_API_KEY"),
-        api_base=os.getenv("BONZAI_URL"),
-        custom_llm_provider="openai"
+        api_base=os.getenv("BONZAI_URL")
     )
 
     cms_config = {
@@ -50,14 +53,12 @@ def initialize_config():
 
 
 config = initialize_config()
-
-
 # endregion
 
 # region agentic tools
 @tool
-def personalize_marketing_text(generic_blocks:str, customer_background:str) -> str:
-    """Personalize marketing text based on generic content and customer background."""
+def personalize_texts(generic_blocks:str, customer_background:str) -> str:
+    """Personalize marketing text based on generic content and customer's background."""
     print("Generating personalized text for block(s)...")
     personalized_blocks = config["llm"].invoke(f"""Generate a new marketing text for the provided generic blocks, based on the content that is currently in there, by tailoring it to the customer's background. 
             It is very important to keep in mind that you may not twist the meaning of the content. While it should be a text that shows how the product can fit in the customer's interests, it should still advertise the product the content states.
@@ -73,22 +74,24 @@ def personalize_marketing_text(generic_blocks:str, customer_background:str) -> s
 
 
 @tool
-def personalize_hero_image(generic_blocks: str, customer_background: str) -> str:
-    """Personalize the hero image of a page based on the customer background."""
-    # TODO: Implement personalizing the hero image
-    '''print("Generating personalized text for block(s)...")
-    personalized_blocks = config["llm"].invoke(f"""Generate a new marketing text for the provided generic blocks, based on the content that is currently in there, by tailoring it to the customer's background. 
-            It is very important to keep in mind that you may not twist the meaning of the content. While it should be a text that shows how the product can fit in the customer's interests, it should still advertise the product the content states.
-            Use the received block(s) as a formatting structure and return new block(s) with the generated text. Make sure that you do not change or add anything besides the personalized text. 
-            Give only the generated object as a response, nothing else.
+def personalize_images(generic_blocks: str, assets:str, customer_background: str) -> str:
+    """Personalize image(s) of a page based on the customer's background."""
+    print("Personalizing image(s) for block(s)...")
+    personalized_blocks = config["llm"].invoke(f"""Choose an image from the asset list that matches the context of the page and fits with the customer's background. To do this look at the tags, title and description. 
+            Make a top 3 of most suitable images and then analyse the image via the URL. When chosen make sure the image exists in the asset_list. You can use the generic blocks to know what the context of the page is.
+            It is very important that the image always fits with the context of the page, if you cannot find an image that suits the customer's background then choose an image that suits the context best.
+            Use the received block(s) as a formatting structure and return new block(s) with the correct image information of the new image. Make sure that you do not change or add anything besides the image information and 
+            that you copy all information of the chosen image from the asset list to the new block(s), this includes the uid, is_dir, version, ACL, content_type, created_at, created_by, description, file_size, filename, 
+            parent_uid, tags, title, updated_at, updated_by, publish_details, url and layout. Give only the generated object as a response, nothing else.
 
             Information you can use:
             Generic blocks: {generic_blocks}
-            customer_background: {customer_background}""")'''
+            Customer_background: {customer_background}
+            Asset_list: {assets}""")
 
-    return
+    return str(personalized_blocks)
 
-tool
+@tool
 def personalize_element_order(generic_blocks: str, customer_background: str) -> str:
     """Personalize the order of elements of a page, based on the customer background."""
     # TODO: Implement personalizing the order of elements
@@ -104,20 +107,21 @@ def personalize_element_order(generic_blocks: str, customer_background: str) -> 
 
     return
 
-tools = [personalize_marketing_text, personalize_hero_image, personalize_element_order]
-
-
+tools = [personalize_texts, personalize_images, personalize_element_order]
 # endregion
 
 # region Langgraph nodes
 class AgentState(TypedDict):
-    generated_page: str
+    messages: Annotated[Sequence[BaseMessage], add_messages]
     page_list: dict
+    asset_list: dict
     content_type_uid: str
+    generated_page: str
 
 
-def retrieve_pages_node(state: AgentState):
-    """Retrieve all existing pages from ContentStack"""
+def fetch_data_node(state: AgentState):
+    """Retrieve all pages and assets from ContentStack."""
+    # Create API call
     headers = {
         "api_key": config["cms"]["api_key"],
         "access_token": config["cms"]["delivery_token"],
@@ -129,32 +133,49 @@ def retrieve_pages_node(state: AgentState):
         "include[]": "header_reference"
     }
 
-    url = f"https://{config['cms']['base_url']}/v3/content_types/{state['content_type_uid']}/entries"
+    retrieve_pages_url = f"https://{config['cms']['base_url']}/v3/content_types/{state['content_type_uid']}/entries"
+    retrieve_assets_url = f"https://{config['cms']['base_url']}/v3/assets"
 
+    # Retrieve pages from ContentStack
     try:
-        response = requests.get(url, headers=headers, params=params)
+        response = requests.get(retrieve_pages_url, headers=headers, params=params)
         response.raise_for_status()
         page_list = response.json()
 
         state['page_list'] = page_list
         print("Retrieved pages from CMS.")
-        return state
     except requests.exceptions.RequestException as e:
         return f"Error retrieving pages: {str(e)}"
 
+    # Retrieve assets from ContentStack
+    try:
+        response = requests.get(retrieve_assets_url, headers=headers, params=params)
+        response.raise_for_status()
+        asset_list = response.json()
+
+        state['asset_list'] = asset_list
+        print("Retrieved assets from CMS.")
+        return state
+    except requests.exceptions.RequestException as e:
+        return f"Error retrieving assets: {str(e)}"
+
 
 def personalize_page_node(state: AgentState):
-    """Personalize a page"""
+    """Personalize a page to the customer's background."""
     personalizer_agent = create_react_agent(config["llm"], tools)
 
     prompt = {
-        "messages": [("user", f"""Personalize the our services page with personalized marketing text by sending it the blocks of the page. 
-        Use the personalized blocks you will receive to generate a new page object using the page from the page list as a template and filling in the updated content. 
-        Make sure that you do not change or add anything besides the personalized text. Give only the generated object as an answer, nothing else.
+        "messages": [("user", f"""Personalize the our services page by personalizing the text and then the image. Your steps should be as follows:
+        1. Personalize the marketing text using the customer's interests and the page list.
+        2. Use the returned blocks to generate a new page object using the our-services page from the page list as a template and replacing the old text with the updated text. Make sure that you do not change or add anything besides the personalized text.
+        3. Personalize the image using the customer's interests, the page list and the asset list.
+        4. Use the personalized blocks to generate a new page object by using the page from step 2 as a template and filling in the updated image information. Make sure that you do not change or add anything besides the image information.
+         Give only the generated object as an answer, nothing else.
 
             Information you can use:
             Customer interests: The user works at a construction company. 
             Page list: {state['page_list']}
+            Asset list: {state['asset_list']}
             """)]}
 
     # Generate marketing text with agent
@@ -162,7 +183,6 @@ def personalize_page_node(state: AgentState):
         print("Personalizer agent starts personalizing...")
         response = personalizer_agent.invoke(prompt)
         response = response['messages'][-1].content
-        print("Personalizer agent personalized a page.")
 
         # Create json object from the response
         try:
@@ -184,20 +204,18 @@ def personalize_page_node(state: AgentState):
     result = collection.insert_one(generated_page)
     print(f"Saved personalized page in database with ID: {result.inserted_id}")
     return state
-
-
 # endregion
 
 # Create graph
 graph = StateGraph(AgentState)
-graph.add_node("RetrievePages", retrieve_pages_node)
+graph.add_node("FetchData", fetch_data_node)
 graph.add_node("personalizePage", personalize_page_node)
-graph.add_edge(START, "RetrievePages")
-graph.add_edge("RetrievePages", "personalizePage")
+graph.add_edge(START, "FetchData")
+graph.add_edge("FetchData", "personalizePage")
 graph.add_edge("personalizePage", END)
 
-retrievalAgent = graph.compile()
+personalizationAgent = graph.compile()
 
 # Run agent
-result = retrievalAgent.invoke({"content_type_uid": "page"})
-print(result["page_list"])
+result = personalizationAgent.invoke({"content_type_uid": "page"})
+
